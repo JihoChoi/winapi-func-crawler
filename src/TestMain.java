374ee7ef1f60d959866ccd9bd6b05b6190d1768f
Vanessa <Vanessa@Vanessaui-MacBook-Pro.local>
2017-08-01 14:57:49 +0900

    temp main func

---
+++ src/TestMain.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Vanessa on 7/28/17.
+ */
+public class TestMain {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Starting Main");
+        WinAPIFunction winAPIFunction = new WinAPIFunction("");
+    }
+}
